3d8c3eadd5e075725ff8071b97c770ffcf6be289
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-11-02 08:44:23 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,43 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from 'prop-types';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+const SecondsCounter = (props) =>{
+    return (
+        <div className="bg-dark text-center m-3 text-light counter">
+            <div><i className="far fa-clock"></i></div>
+            <div className="five">{props.digitFive % 10}</div>
+            <div className="four">{props.digitFour % 10}</div>
+            <div className="three">{props.digitThree % 10}</div>
+            <div className="two">{props.digitTwo % 10}</div>
+            <div className="one">{props.digitOne % 10}</div>
+            <div>,</div>
+        </div>
+    )
+}
+SecondsCounter.PropTypes = {
+    digitOne: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitFive: PropTypes.number,
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function(){
+    const five = Math.floor(counter/10000);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter ++;
+
+    ReactDOM.render(<SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} />, document.querySelector("#app"));
+},1000)
+
+
