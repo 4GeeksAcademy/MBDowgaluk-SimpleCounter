a7ccb979d9a8bbffb344dba6147aaf4cb6d25fd9
Merlina Barbara Dowgaluk <merlina.dowgaluk@gmail.com>
2023-11-05 12:32:26 +0000

    add simple counter bonus

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,55 @@
 //import react into the bundle
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
-import PropTypes from 'prop-types';
+
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-const SecondsCounter = (props) =>{
+const SecondsCounter = () =>{
+    const [ counter, setCounter ] = useState(0);
+    const [ isActive, setIsActive ] = useState(false);
+
+    const handlePrincipal = () =>{
+        if (isActive){
+            setIsActive(false);
+        } else {
+            setIsActive(true)
+        }
+    };
+    const handleReset = () =>{
+        setCounter(0);
+        setIsActive(false);
+    };
+
+    useEffect(() => {
+        if (isActive){
+            const interval = setInterval(() => {
+                setCounter(counter => counter + 1)
+            }, 100)
+            return () => clearInterval(interval);
+        }
+    }, [isActive])
+
     return (
         <div className="bg-dark text-center m-3 text-light counter">
             <div><i className="far fa-clock"></i></div>
-            <div className="five">{props.digitFive % 10}</div>
-            <div className="four">{props.digitFour % 10}</div>
-            <div className="three">{props.digitThree % 10}</div>
-            <div className="two">{props.digitTwo % 10}</div>
-            <div className="one">{props.digitOne % 10}</div>
+            <div className="five">{Math.floor(counter/100000) % 10}</div>
+            <div className="four">{Math.floor(counter/10000) % 10}</div>
+            <div className="three">{Math.floor(counter/1000) % 10}</div>
+            <div className="two">{Math.floor(counter/100) % 10}</div>
+            <div className="one">{Math.floor(counter/10) % 10}</div>
             <div>,</div>
-            <div className="decimal">{props.digitDecimal % 10}</div>
+            <div className="decimal">{Math.floor(counter/1) % 10}</div>
+            <div class="btn-group-vertical" role="group" aria-label="Vertical button group">
+                <button type="button" class="btn btn-outline-success" onClick={handlePrincipal}>{isActive ? 'Pause' : 'Start'}</button>
+                <button type="button" class="btn btn-outline-danger" onClick={handleReset}>Reset</button>
+            </div>
         </div>
     )
 }
-SecondsCounter.PropTypes = {
-    digitOne: PropTypes.number,
-    digitTwo: PropTypes.number,
-    digitThree: PropTypes.number,
-    digitFour: PropTypes.number,
-    digitFive: PropTypes.number,
-    digitDecimal: PropTypes.number,
-}
 
-let counter = 0;
-setInterval(function(){
-    const five = Math.floor(counter/100000);
-    const four = Math.floor(counter/10000);
-    const three = Math.floor(counter/1000);
-    const two = Math.floor(counter/100);
-    const one = Math.floor(counter/10);
-    const decimal = Math.floor(counter/1);
-    counter ++;
-
-    ReactDOM.render(<SecondsCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitDecimal={decimal} />, document.querySelector("#app"));
-},100)
+ReactDOM.render(<SecondsCounter  />, document.querySelector("#app"))
 
 
